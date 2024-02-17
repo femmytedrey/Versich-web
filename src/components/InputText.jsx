c748f9c66ad9d1c4f5b6e05612049da6a439872a
Tony Michael <tee.jhay1@gmail.com>
2024-02-17 16:11:34 +0100

    login page

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,17 +1,45 @@
-import { BsFillEyeSlashFill } from "react-icons/bs";
+import { useState } from "react";
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
+/**
+ * Custom input component that toggles between password visibility.
+ *
+ * Uses the useState hook to manage password visibility state.
+ * Renders an input with type "password" or "text" based on visibility state.
+ * Shows eye icon to toggle visibility on click.
+ */
+
+const InputText = ({ label, inputType }) => {
+  const [showPassword, setShowPassword] = useState(false);
 
 
-const InputText = () => {
+  const togglePassword = () => {
+    setShowPassword(prev => !prev);
+  }
+
+
   return (
-    <div className="w-full">
+    <div className="space-y-2">
+      {/* label */}
       <div className="flex items-center justify-between">
-        <label htmlFor="">Label</label>
-        <button className="flex items-center space-x-2">
-          <BsFillEyeSlashFill />
-          <span>Hide</span>
-        </button>
+        <label htmlFor="" className="text-versich-label text-sm">{label}</label>
+        {inputType === 'password' &&
+          <button className="flex items-center space-x-2 text-sm" onClick={togglePassword} >
+            {showPassword ? (
+              <BsFillEyeFill className="text-versich-label" />
+            ) : (
+              <BsFillEyeSlashFill className="text-versich-label" />
+            )}
+            <span className="text-versich-label">
+              {showPassword ? 'Hide' : 'Show'}
+            </span>
+          </button>
+        }
       </div>
-      <input type="text"  className="w-full h-10 rounded-md border-2 border-gray-200" />
+      {/* <input type={inputType} className="w-full h-10 rounded-md border-[1px] border-versich-border" /> */}
+      <input
+        type={showPassword ? 'text' : inputType}
+        className="w-full h-10 rounded-md border-[1px] border-versich-border px-3"
+      />
     </div>
   );
 }
