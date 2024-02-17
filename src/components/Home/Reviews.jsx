4a1ae6546578ce388c664ea3aa9c3dec3e6c0581
femmytedrey <femmytedrey@gmail.com>
2024-02-17 06:30:07 +0100

    in progress

--- src/components/Home/Reviews.jsx
+++ src/components/Home/Reviews.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Testimony from './Testimony';
+import testifiers from '../../assets/Testifiers';
 
 const Reviews = () => {
   return (
-    <div>
-      Reviews
+    <div className='my-16'>
+      <p className='text-4xl font-bold text-versich-darktext-color'>Reviews</p>
+      <div>
+        <Testimony testifiers={testifiers}/>
+      </div>
     </div>
   );
 }
