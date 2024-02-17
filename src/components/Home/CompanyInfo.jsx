f9efcdcdee5e0b9c8708b74ecca56684949802ab
femmytedrey <femmytedrey@gmail.com>
2024-02-17 12:04:05 +0100

    in progress

--- src/components/Home/CompanyInfo.jsx
+++ src/components/Home/CompanyInfo.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 const CompanyInfo = () => {
   return (
     <div>
-      <p className='pb-4 font-bold text-xl'>Company Info</p>
-      <ul className='flex flex-col gap-y-3 text-[#BDBDBD] font-bold'>
-        <li>
+      <p className='pb-4 font-bold text-lg'>Company Info</p>
+      <ul className='flex flex-col gap-y-3 text-md text-[#BDBDBD] font-bold'>
+        <li className='hover:text-[#ECECEC]'>
             <a href="#">Website & development</a>
         </li>
-        <li>
+        <li className='hover:text-[#ECECEC]'>
             <a href="#">Data  & Techonolgy</a>
         </li>
-        <li>
+        <li className='hover:text-[#ECECEC]'>
             <a href="#">Finance Transformation</a>
         </li>
-        <li>
+        <li className='hover:text-[#ECECEC]'>
             <a href="#">Cloud Computing</a>
         </li>
       </ul>
