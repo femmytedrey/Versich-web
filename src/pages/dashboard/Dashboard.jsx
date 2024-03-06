62782d843a43132656934db07e6d1b3defe5a52d
femmytedrey <femmytedrey@gmail.com>
2024-03-06 15:41:41 +0100

    previewing dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -22,8 +22,8 @@ const Dashboard = () => {
 
   return (
     <div className="bg-versich-primary-bg mx-10 md:mx-16 lg:mx-28 space-y-4 py-10">
-      <div className="bg-white w-full px-4 font-semibold py-6 text-start shadow-lg text-versich-dark-blue rounded-xl flex justify-between">
-        <p>Good Afternoon, {user.first_name}!</p>
+      <div className="bg-white w-full px-4 font-semibold py-6 text-start shadow-lg text-versich-dark-blue rounded-xl flex justify-between sm:flex-col">
+        <p className="mb-0 sm:mb-2">Good Afternoon, {user.first_name}!</p>
         <Link
          to="/auth/verification/YOUR_TOKEN/email/"
          className="text-versich-blue underline hover:text-versich-blue-hover"
