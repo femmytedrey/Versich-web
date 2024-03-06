089312d34573caec9f700b742fc643ed8cc6aad4
femmytedrey <femmytedrey@gmail.com>
2024-03-06 09:37:04 +0100

    adding countdown for the next click

--- src/pages/EmailVerification.jsx
+++ src/pages/EmailVerification.jsx
@@ -1,26 +1,46 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const EmailVerification = () => {
-    const emailResend = () => {
-        return alert('Email resent')
+  const [countdown, setCountdown] = useState(30);
+  const [showCountdown, setShowCountdown] = useState(false);
+
+  const emailResend = () => {
+    alert('Email resent');
+    setShowCountdown(true);
+  };
+
+  useEffect(() => {
+    let timer;
+    if (showCountdown && countdown > 0) {
+      timer = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
     }
+    return () => clearInterval(timer);
+  }, [countdown, showCountdown]);
+
   return (
-    <div className="py-10 md:py-14 px-3 mb-12 overflow-hidden flex justify-center  bg-versich-primary-bg items-center">
-        <div className="w-full bg-white shadow-md py-5 md:py-10 px-3 md:px-10 max-w-[580px] rounded-md">
-            <h2 className="font-bold text-2xl mb-5">Email Verification</h2>
-            <p className="text-center mb-5">
-                Kindly check your email inbox or spam folder to verify your email address.
-            </p>
-            <p className="text-center">
-                Click{' '}
-                <button className="text-versich-blue underline" onClick={emailResend}>
-                here
-                </button>{' '}
-                to resend.
-            </p>
-        </div>
+    <div className="py-10 md:py-14 px-3 mb-12 overflow-hidden flex justify-center bg-versich-primary-bg items-center">
+      <div className="w-full bg-white shadow-md py-5 md:py-10 px-3 md:px-10 max-w-[580px] rounded-md">
+        <h2 className="font-bold text-2xl mb-5">Email Verification</h2>
+        <p className="text-center mb-5">
+          Kindly check your email inbox or spam folder to verify your email address.
+        </p>
+        <p className="text-center">
+          Click{' '}
+          <button className="text-versich-blue underline" onClick={emailResend}>
+            here
+          </button>{' '}
+          to resend.
+        </p>
+        {showCountdown && (
+          <p className="text-center">
+            Resend email in {countdown}s.
+          </p>
+        )}
+      </div>
     </div>
-    
   );
 }
 
