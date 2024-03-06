144fa66353ebfc322538a6760f04f9f7e98aab69
femmytedrey <femmytedrey@gmail.com>
2024-03-06 10:35:33 +0100

    Styling email Icon

--- src/pages/EmailVerified.jsx
+++ src/pages/EmailVerified.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React from "react";
 import { MdOutlineMarkEmailRead } from "react-icons/md";
-
 
 const EmailVerified = () => {
   return (
@@ -8,15 +7,14 @@ const EmailVerified = () => {
       <div className="w-full bg-white shadow-md py-5 md:py-10 px-3 md:px-10 max-w-[580px] rounded-md">
         <h2 className="font-bold text-2xl mb-5">Email Verified</h2>
         <div className="text-6xl py-3 flex justify-center text-versich-blue">
-            <MdMarkEmailUnread />
+          <MdOutlineMarkEmailRead />
         </div>
         <p className="text-center mb-5">
           Your email has been successfully verified. Thank you!
         </p>
-        
       </div>
     </div>
   );
-}
+};
 
 export default EmailVerified;
