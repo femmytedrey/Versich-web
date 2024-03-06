7c3929bd5d3510364d90e95aa197d1729f2fcdfb
femmytedrey <femmytedrey@gmail.com>
2024-03-06 17:54:35 +0100

    Profile completion handling on progress bar

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -3,3 +3,4 @@ export const homePath = "/"
 export const dashboardPath = "/dashboard/"
 export const signupPath = "/auth/signup/"
 export const loginPath = "/auth/login/"
+export const initials = `${user.first_name.charAt(0)}${user.last_name.charAt(0)}`;
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -2,9 +2,11 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import CircularProgressBar from "../../components/CircularProgressBar";
 import ConfirmButton from "../../components/Buttons/ConfirmButton";
+import { initials } from "../../assets/constants";
 
 const Dashboard = () => {
   const { user } = useSelector((state) => state.auth);
+  const [progress, setProgress] = useState(45);
 
   if (
     !user ||
@@ -23,8 +25,7 @@ const Dashboard = () => {
     console.log("Testing Edit btn");
   };
 
-  const initials = `${user.first_name.charAt(0)}${user.last_name.charAt(0)}`;
-  const progress = 25;
+  
 
   return (
     <div className="bg-versich-primary-bg mx-10 md:mx-16 lg:mx-28 space-y-4 py-10">
@@ -52,14 +53,21 @@ const Dashboard = () => {
               <CircularProgressBar progress={progress} />
             </div>
             <div>
-              <p>
-               Complete your profile {' '}
-                 <span>
-                   <Link to="/steppers" className="text-versich-blue underline">
-                     here
-                   </Link>
-                 </span>
-              </p>
+              {progress < 100 ? (
+                <p>
+                  Complete your profile{" "}
+                  <span>
+                    <Link
+                      to="/steppers"
+                      className="text-versich-blue underline"
+                    >
+                      here
+                    </Link>
+                  </span>
+                </p>
+              ) : (
+                <p>Your profile is completed.</p>
+              )}
             </div>
           </div>
           <div>
