f3cf4661c5811e71d840b546386f77bbb165214c
femmytedrey <femmytedrey@gmail.com>
2024-03-06 09:21:05 +0100

    Email resend

--- src/pages/EmailVerification.jsx
+++ src/pages/EmailVerification.jsx
@@ -1,20 +1,26 @@
 import { Link } from "react-router-dom";
 
 const EmailVerification = () => {
+    const emailResend = () => {
+        return alert('Email resent')
+    }
   return (
-    <div className="flex flex-col items-center justify-center my-10 min-h-80">
-      <h2 className="font-bold text-2xl mb-5">Email Verification</h2>
-      <p className="text-center mb-5">
-        An email verification has been sent to your email address. Please click the link in the email to verify your account.
-      </p>
-      <p className="text-center">
-        If you didn't receive any email, you can click{' '}
-        <Link to="/auth/resend-verification" className="text-versich-blue underline">
-          here
-        </Link>{' '}
-        to resend the verification email.
-      </p>
+    <div className="py-10 md:py-14 px-3 mb-12 overflow-hidden flex justify-center  bg-versich-primary-bg items-center">
+        <div className="w-full bg-white shadow-md py-5 md:py-10 px-3 md:px-10 max-w-[580px] rounded-md">
+            <h2 className="font-bold text-2xl mb-5">Email Verification</h2>
+            <p className="text-center mb-5">
+                Kindly check your email inbox or spam folder to verify your email address.
+            </p>
+            <p className="text-center">
+                If you didn't receive any email, you can click{' '}
+                <Link to="/auth/resend-verification" className="text-versich-blue underline" onClick={emailResend}>
+                here
+                </Link>{' '}
+                to resend the verification email.
+            </p>
+        </div>
     </div>
+    
   );
 }
 
