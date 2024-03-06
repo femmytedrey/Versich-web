98aac00f2c046141bacc4e5c94b201ba498f136e
femmytedrey <femmytedrey@gmail.com>
2024-03-06 10:31:21 +0100

    Add Email Icon

--- src/pages/EmailVerification.jsx
+++ src/pages/EmailVerification.jsx
@@ -35,7 +35,7 @@ const EmailVerification = () => {
     <div className="py-10 md:py-14 px-3 mb-12 overflow-hidden flex justify-center bg-versich-primary-bg items-center">
       <div className="w-full bg-white shadow-md py-5 md:py-10 px-3 md:px-10 max-w-[580px] rounded-md">
         <h2 className="font-bold text-2xl mb-5">Email Verification</h2>
-        <div className="text-3xl py-3 flex justify-center text-versich-blue">
+        <div className="text-6xl py-3 flex justify-center text-versich-blue">
             <MdMarkEmailUnread />
         </div>
         <p className="text-center mb-5">
--- src/pages/EmailVerified.jsx
+++ src/pages/EmailVerified.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { MdOutlineMarkEmailRead } from "react-icons/md";
+
 
 const EmailVerified = () => {
   return (
     <div className="py-10 md:py-14 px-3 mb-12 overflow-hidden flex justify-center bg-versich-primary-bg items-center">
       <div className="w-full bg-white shadow-md py-5 md:py-10 px-3 md:px-10 max-w-[580px] rounded-md">
         <h2 className="font-bold text-2xl mb-5">Email Verified</h2>
+        <div className="text-6xl py-3 flex justify-center text-versich-blue">
+            <MdMarkEmailUnread />
+        </div>
         <p className="text-center mb-5">
           Your email has been successfully verified. Thank you!
         </p>
