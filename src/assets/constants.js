8e8af73700471cd17d1f6fde8933f3421e92b3f5
Kumara2mahe <mahendrakumara268@gmail.com>
2024-03-20 13:15:01 +0530

    Make the userselection look exact same as the design

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -4,3 +4,7 @@ export const dashboardPath = "/dashboard/"
 export const signupPath = "/auth/signup/"
 export const loginPath = "/auth/login/"
 export const accountTypePath = "/as"
+
+// Accounts
+export const BUYER = "buyer"
+export const SELLER = "seller"
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -74,18 +74,15 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`flex justify-between items-center py-6 px-10 md:px-16 lg:px-28 md:px-25 relative ${
-        isHome ? "bg-transparent" : "bg-white"
-      } `}
+      className={`flex justify-between items-center py-6 max-[400px]:px-6 px-10 md:px-16 lg:px-28 md:px-25 relative ${isHome ? "bg-transparent" : "bg-white"
+        } `}
     >
       <div>
         <Link to={homePath} className="flex items-center justify-center">
           <img src={logo} alt="logo" className="w-[48px] md:w-[52px]" />
           &nbsp;
           <span
-            className={`font-semibold text-xl  ${
-              isHome ? "text-white" : "text-black"
-            }`}
+            className={`font-semibold text-xl  ${isHome ? "text-white" : "text-black"}`}
           >
             VersiMarket
           </span>
@@ -94,11 +91,10 @@ const Navbar = () => {
       <div>
         <div
           className={`md:flex md:gap-x-7 flex-col md:flex-row shadow-lg md:shadow-none absolute md:relative bg-white bg-opacity-80 backdrop-filter backdrop-blur-md md:bg-transparent md:mt-0 w-full left-0 px-10 md:px-0 py-4 md:py-0  md:visible
-          ${
-            mobileMenu
+          ${mobileMenu
               ? "top-[100%] visible  duration-500 transition-all ease-in"
               : "hidden  top-[-100%] duration-500 transition-all ease-in"
-          }`}
+            }`}
         >
           {/* <div
             className="relative inline-block text-left w-full md:w-auto"
@@ -146,11 +142,10 @@ const Navbar = () => {
                 to={dashboardPath}
                 type="button"
                 onClick={handleClick}
-                className={`px-4 md:px-0 py-3 md:py-2 text-lg font-regular hover:text-versich-blue ${
-                  isHome
-                    ? "text-versich-darktext-color md:text-white"
-                    : "text-versich-darktext-color md:text-versich-darktext-color"
-                }`}
+                className={`px-4 md:px-0 py-3 md:py-2 text-lg font-regular hover:text-versich-blue ${isHome
+                  ? "text-versich-darktext-color md:text-white"
+                  : "text-versich-darktext-color md:text-versich-darktext-color"
+                  }`}
               >
                 Dashboard
               </Link>
@@ -171,11 +166,10 @@ const Navbar = () => {
                 to={loginPath}
                 type="button"
                 onClick={handleClick}
-                className={`px-4 md:px-0 py-3 md:py-2 text-lg font-regular hover:text-versich-blue ${
-                  isHome
-                    ? "text-versich-darktext-color md:text-white"
-                    : "text-versich-darktext-color md:text-versich-darktext-color"
-                }`}
+                className={`px-4 md:px-0 py-3 md:py-2 text-lg font-regular hover:text-versich-blue ${isHome
+                  ? "text-versich-darktext-color md:text-white"
+                  : "text-versich-darktext-color md:text-versich-darktext-color"
+                  }`}
               >
                 Login
               </Link>
@@ -195,18 +189,16 @@ const Navbar = () => {
       {/* {mobileMenu ? 'open' : 'close'} */}
       {mobileMenu ? (
         <div
-          className={`cursor-pointer md:hidden ${
-            isHome ? "text-white" : "text-versich-darktext-color"
-          }`}
+          className={`cursor-pointer md:hidden ${isHome ? "text-white" : "text-versich-darktext-color"
+            }`}
           onClick={menu}
         >
           <MdClose className="text-3xl" />
         </div>
       ) : (
         <div
-          className={`cursor-pointer md:hidden ${
-            isHome ? "text-white" : "text-versich-darktext-color"
-          }`}
+          className={`cursor-pointer md:hidden ${isHome ? "text-white" : "text-versich-darktext-color"
+            }`}
           onClick={menu}
         >
           <RxHamburgerMenu className="text-3xl" />
