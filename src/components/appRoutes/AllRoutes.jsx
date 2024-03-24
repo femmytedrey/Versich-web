9cdfaade6e30141854631a44d330011671f993b9
femmytedrey <femmytedrey@gmail.com>
2024-03-24 21:59:03 +0100

    accessing account type from auth

--- src/components/appRoutes/AllRoutes.jsx
+++ src/components/appRoutes/AllRoutes.jsx
@@ -24,6 +24,7 @@ import { useEffect } from "react";
 
 const AllRoutes = ({ checkAuth, isAuthenticated, user }) => {
     const userType = user && user.account_type;
+    console.log(userType, 'This is the current userType')
   useEffect(() => {
     checkAuth();
   }, [checkAuth, user]);
