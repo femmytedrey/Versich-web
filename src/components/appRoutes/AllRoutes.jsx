41fa6e85897ce0c772e857f15f7fc3bf4c0fc1b9
femmytedrey <femmytedrey@gmail.com>
2024-03-27 07:26:19 +0100

    extracting token from the received link

--- src/components/appRoutes/AllRoutes.jsx
+++ src/components/appRoutes/AllRoutes.jsx
@@ -35,7 +35,7 @@ const AllRoutes = () => {
           />
         </Route>
         <Route element={<AuthRoutes />}>
-          <Route path="verify-email/:token" element={<VerifyEmailPage />} />
+          {/* <Route path="verify-email/:token" element={<VerifyEmailPage />} /> */}
 
           {/* This EmailVerification is for the resend */}
           <Route
--- src/pages/auth/verification/VerifyEmailPage.jsx
+++ src/pages/auth/verification/VerifyEmailPage.jsx
@@ -1,40 +1,11 @@
-import { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import React from 'react';
 
-import { verifyEmail } from '../../../api';
-
-
-export default function VerifyEmailPage() {
-
-  const [error, setError] = useState(null);
-
-  const { token } = useParams();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const verifyToken = async () => {
-      try {
-        await verifyEmail(token);
-        console.log("Token Extracted Successfully")
-        navigate('verification/email/');
-      } catch (error) {
-        console.error(error);
-        setError(error.message);
-        navigate('verification/:token/email/');
-      } finally {
-        console.log('Code block is executed successfully')
-      }
-    }
-    verifyToken();
-  }, [token, navigate]);
-
-
-
+const VerifyEmailPage = () => {
   return (
     <div>
-      <h1>Verify Email...</h1>
-      {error && <p>{error}</p>}
+      
     </div>
   );
-
 }
+
+export default VerifyEmailPage;
