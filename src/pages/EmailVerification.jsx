92479bc161f12723a92e66db6dc401a3c2e5c999
femmytedrey <femmytedrey@gmail.com>
2024-03-27 08:58:13 +0100

    fix route and checking on email resend

--- src/pages/EmailVerification.jsx
+++ src/pages/EmailVerification.jsx
@@ -8,20 +8,21 @@ const EmailVerification = () => {
   const [countdown, setCountdown] = useState(30);
   const [showCountdown, setShowCountdown] = useState(false);
   const [disableResend, setDisableResend] = useState(false);
+  const [resendError, setResendError] = useState(null);
 
   const emailResend = async () => {
     try {
       await resendVerificationEmail();
-  
+
       alert("Email resent");
       setShowCountdown(true);
       setDisableResend(true);
       setCountdown(30);
     } catch (error) {
       console.error("Error resending email:", error);
+      setResendError("Error resending email. Please try again later.");
     }
   };
-  
 
   useEffect(() => {
     let timer;
@@ -42,11 +43,14 @@ const EmailVerification = () => {
 
   return (
     <div className="py-10 md:py-14 px-3 mb-12 overflow-hidden flex justify-center bg-versich-primary-bg items-center">
-      <Meta title='Email Verification' description='Proceed to verify your email address' />
+      <Meta
+        title="Email Verification"
+        description="Proceed to verify your email address"
+      />
       <div className="w-full bg-white shadow-md py-5 md:py-10 px-3 md:px-10 max-w-[580px] rounded-md">
         <h2 className="font-bold text-2xl mb-5">Email Verification</h2>
         <div className="text-6xl pb-5 flex justify-center text-versich-blue">
-            <MdMarkEmailUnread />
+          <MdMarkEmailUnread />
         </div>
         <p className="text-center mb-5">
           Kindly check your email inbox or spam folder to verify your email
@@ -63,6 +67,7 @@ const EmailVerification = () => {
           </button>{" "}
           to resend Verification Link.
         </p>
+        {resendError && <p className="text-center text-red-500">{resendError}</p>}
         {showCountdown && (
           <p className="text-center">Resend email in {countdown}s.</p>
         )}
