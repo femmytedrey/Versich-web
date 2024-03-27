af1ba6750131979faf956b2d437d0635b1fd3b84
femmytedrey <femmytedrey@gmail.com>
2024-03-27 09:00:21 +0100

    fix route and checking on email resend

--- src/pages/auth/verification/VerifyEmailPage.jsx
+++ src/pages/auth/verification/VerifyEmailPage.jsx
@@ -16,11 +16,11 @@ export default function VerifyEmailPage() {
       try {
         await verifyEmail(token);
         console.log("Token Extracted Successfully")
-        navigate('/verification/email-verified', { replace: true });
+        navigate('/auth/verification/email-verified', { replace: true });
       } catch (error) {
         console.error(error);
         setError(error.message);
-        navigate('/verification/resend-email/', { replace: true });
+        navigate('/auth/verification/resend-email/', { replace: true });
       } finally {
         console.log('Code block is executed successful')
       }
