1b848fe6898d0e771802333850eb520e284caea0
femmytedrey <femmytedrey@gmail.com>
2024-03-29 15:48:31 +0100

    Create intentional error to test Email Verified  Error Handling

--- src/pages/auth/verification/VerifyEmailPage.jsx
+++ src/pages/auth/verification/VerifyEmailPage.jsx
@@ -1,44 +1,38 @@
-import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import {  useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
-import EmailVerified from "../../EmailVerified";
-import { useDispatch } from "react-redux";
-import { VerifyEmailAction } from "../../../actions/VerifyEmailAction";
+import { verifyEmail } from '../../../api';
+import EmailVerified from '../../EmailVerified';
+
 
 export default function VerifyEmailPage() {
-  const [isVerified, setIsVerified] = useState(false);
-  const [statusText, setStatusText] = useState("");
+    const [isVerified, setIsVerified] = useState(false);
+    const [statusText, setStatusText] = useState('')
 
   const { token } = useParams();
   const navigate = useNavigate();
-  const dispatch = useDispatch();
 
   useEffect(() => {
     const verifyToken = async () => {
       try {
-        await dispatch(VerifyEmailAction(token));
-        if (!isVerified) {
-          setStatusText("Error verifying this email");
+        await verifyEmail(token);
+        if(!isVerified){
+            setStatusText('Error verifying this email')
         }
-        console.log("Token Extracted Successfully");
+        console.log("Token Extracted Successfully")
         setIsVerified(true);
-        setStatusText("Your email has been verified successfully!");
+        setStatusText('Your email has been verified successfully!')
       } catch (error) {
         console.error(error);
-        const message = error?.message
-          ? JSON.parse(error.message)?.message ||
-            "An error occurred while verifying the email."
-          : "An error occurred while verifying the email.";
-        setStatusText(message);
-        if (error.response && error.response.status === 404) {
-            navigate("/auth/verification/resend-email/", { replace: true });
-        }
+        // navigate('/auth/verification/resend-email/', { replace: true });
       } finally {
-        console.log("Code block is executed successful");
+        console.log('Code block is executed successful')
       }
-    };
+    }
     verifyToken();
   }, [token, navigate]);
+
+
 
   return (
     <div>
@@ -49,4 +43,5 @@ export default function VerifyEmailPage() {
       )}
     </div>
   );
+
 }
