06cc987a3cbd1d83064737ff2823a34c96b5cedc
femmytedrey <femmytedrey@gmail.com>
2024-04-01 17:17:59 +0100

    Resend Email Verification API with getReqConfig() for user auth

--- src/actions/resendVerificationEmailAction.js
+++ src/actions/resendVerificationEmailAction.js
@@ -1,9 +1,9 @@
 import { resendVerificationEmail } from "../api";
 
-export const resendVerificationEmailAction = (csrfToken) => {
+export const resendVerificationEmailAction = (token) => {
   return async (dispatch) => {
     try {
-      await resendVerificationEmail(csrfToken);
+      await resendVerificationEmail(token);
     } catch (error) {
       throw error;
     }
--- src/api/index.js
+++ src/api/index.js
@@ -32,10 +32,10 @@ export const verifyEmail = (token) => {
   return API.get(`/auth/verify/account/${token}`);
 };
 
-export const resendVerificationEmail = (csrfToken) => {
+export const resendVerificationEmail = (token) => {
   return API.post(
     "/auth/verify/account/resend/email/",
     null,
-    getReqConfig(csrfToken)
+    getReqConfig(token)
   );
 };
